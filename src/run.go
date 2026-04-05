0ab1e1723d9901cf7fb2a9353475f535bc0c5701
esalama01 <mehdisalama61@gmail.com>
2026-04-05 20:42:50 +0100

    finished step 2

--- src/run.go
+++ src/run.go
@@ -1,27 +1,48 @@
 package src
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"syscall"
 )
 
-func RunCommand(name string, args ...string) (string, error) {
-	ChildArgs := append([]string{"Child_RunCommand", name}, args...)
-	cmd := exec.Command("/proc/self/exe", ChildArgs...) //the /proc/.. is used to spawn the child process that initializes the UTS mnamespace before executing this (main Run) command. 
-	out, err := cmd.CombinedOutput() //CombinedOutput runs the command and returns its combined standard output and standard error.
+func Run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...) //the /proc/.. is used to spawn the child process that initializes the UTS mnamespace before executing this (main Run) command.
+	//cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+
+	 
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS, // the UTS clone call isolates the hostname.
+		Cloneflags: syscall.CLONE_NEWUTS, // the UTS clone call isolates the hostname. and new_user grants permission.
+		//area for improvement: add cap_sys_admin.
 	}
-	return string(out), err
+
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
 }
 
-func Child_RunCommand(name string, args ...string) (string, error) {
-	cmd := exec.Command(name, args...)
-	out, err := cmd.CombinedOutput() //CombinedOutput runs the command and returns its combined standard output and standard error.
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS, // the UTS clone call isolates the hostname.
-	}
+func Child() {
+	err := syscall.Sethostname([]byte("new_container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	//CombinedOutput runs the command and returns its combined standard output and standard error.
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	//Setting the hostname
-	syscall.Sethostname([]byte("container"))
-	return string(out), err
+	if err != nil {
+		panic(err)
+	}
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Hostname updated successfully!")
 }
--- uDocker.go
+++ uDocker.go
@@ -9,13 +9,10 @@ import (
 func main() {
 	args := os.Args
 	switch args[1] {
-	case "run":
-		var arguments []string
-		for _, arg := range args[3:] {
-			arguments = append(arguments, arg)
-		}
-		output, _ := src.RunCommand(args[2], arguments...)
-		fmt.Println(output)
+	case "run": //to run you need to grant the binary root access.
+		src.Run()
+	case "child":
+		src.Child()
 	default:
 		fmt.Println("Invalid Command")
 	}
