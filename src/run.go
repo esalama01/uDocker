d54156d57d6ae4bc4018268fed31e6d3616b3d42
esalama01 <mehdisalama61@gmail.com>
2026-04-05 21:49:32 +0100

    added the mount namespac3e

--- src/run.go
+++ src/run.go
@@ -7,6 +7,9 @@ import (
 	"syscall"
 )
 
+
+
+
 func Run() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...) //the /proc/.. is used to spawn the child process that initializes the UTS mnamespace before executing this (main Run) command.
 	//cmd := exec.Command(os.Args[2], os.Args[3:]...)
@@ -17,7 +20,10 @@ func Run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS, // the UTS clone call isolates the hostname. and new_user grants permission.
+		Cloneflags: syscall.CLONE_NEWUTS | //the UTS clone call isolates the hostname
+		
+		 syscall.CLONE_NEWNS, //The mount (NEWNS) cllonbe sys call isolates the mount points. --> leads to changiing teh root filesystem.
+
 		//area for improvement: add cap_sys_admin.
 	}
 
